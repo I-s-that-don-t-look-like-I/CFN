d6b2b94c91a8c0433ae8fb2193c09f0198f5d06e
yyyljy <yyyljy@gmail.com>
2022-12-10 19:31:13 +0900

    notebook

--- contracts/crowdfund.sol
+++ contracts/crowdfund.sol
@@ -7,12 +7,25 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract CrowdfundContract is ERC721Enumerable, Ownable {
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    address owner;
+    constructor(string memory _name, string memory _symbol, address _idleContract) ERC721(_name, _symbol) {
+        owner = _idleContract;
+    }
+
+    uint index;
 
     struct Crowdfund {
+        uint index;
         string film_name;
         address director;
-        
-        
+        uint target_amount;
+        uint start_time;
+        uint end_time;
+        string imgUrl;
+        STATUS status;
+        uint min_amount;
+        uint max_amount;
     }
+    enum STATUS {PENDING, FUNDING, DONE, SUCCESS, FAILURE}
+    // mapping()
 }
